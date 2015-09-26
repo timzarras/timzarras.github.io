4870e8364b1d6dcd1d007d2133238cc4d5be6700
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2015-09-26 19:01:17 -0400

    yo

--- test/js/timscript.js
+++ test/js/timscript.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         var target = this.hash,
             $target = $(target);
         $('html,body').stop().animate({
-            'scrollTop': $target.offset().top - 79
+            'scrollTop': $target.offset().top - 78
         }, 750, function () {
             window.location.hash = target;
         });
