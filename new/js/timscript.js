6d99beb3ae37210d00850be585ce32d30314e726
tzarras <TimKitchen@Tims-MBP.home>
2016-07-26 00:44:33 -0400

    yo

--- new/js/timscript.js
+++ new/js/timscript.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
 
-
-      $("li",this).click(function() {
-
-        $(this).toggleClass("height");
-
-    });
-
-      $(document).mousemove(function() {
+    
+    $(document).mousemove(function() {
 
         $yo = $("body").width();
+
+        // if cursor is less than 15% from the left, slide list and expand button // 
 
         if (event.pageX < ($yo * .15)) {
 
@@ -23,24 +19,79 @@ $(document).ready(function() {
 
         }
 
-      });
+    });
 
-      $(".expand").click(function() {
+    // expand "this" list element //
 
-        $("li").addClass("height");
+    $("li",this).click(function() {
+
+        var h = $(".info", this).outerHeight();
+        var i = $("img", this).outerHeight();
+
+        // if paragraph is shorter than image, use image height //
+        
+        if (h < i ) {
+          $(this).css("height", i);
+        } else {
+          $(this).css("height", h);
+        }
+
+    });
+
+    // expand all list elements" 
+
+    $(".expand").click(function() {
+
+      // get list element heights //
+      
+        $("li").css("height", function() {
+
+          // compare paragraph to image height again to use larger //
+
+          var h2 = $(".info", this).outerHeight();
+          var i2 = $("img", this).outerHeight();
+          return h2 > i2 ? h2 : i2;
+        });
+
         $(".close").show();
         $(".expand").hide();
 
     });
 
-      $(".close").click(function() {
+    // reset all list element heights //
 
-        $("li").removeClass("height");
+    $(".close").click(function() {
+
+        $("li").css("height","8%");
         $(".close").hide();
         $(".expand").show();
 
-
     });
+
+    // cycle images of the same class //
+
+    $('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').click(function () {
+        $(this).hide();
+        var next = $(this).next();
+        console.log(next.length);
+        if (next.length == 0)
+        next = $(this).parent().find('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').first();
+        next.show();
+    });
+
+    if ($(window).width() < 736) {
+
+      $("li",this).click(function() {
+
+        var h3 = $(".info", this).outerHeight();
+        var i3 = $("img", this).outerHeight();
+        
+          $(this).css("height", h3 + i3);
+       
+        });
+
+
+    }
 
 
 
