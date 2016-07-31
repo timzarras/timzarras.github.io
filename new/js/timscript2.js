1151c00fdfd18863fd302dcd1993ca4cdafbc1c9
tzarras <TimKitchen@Tims-MBP.home>
2016-07-30 20:11:38 -0400

    yo

--- new/js/timscript2.js
+++ new/js/timscript2.js
@@ -3,117 +3,103 @@ $(document).ready(function() {
     var doStuff = (big, small) => {
       if ($(window).width() > 736) big();
       else small();
-    }
+    };
 
     $(window).resize(function() {
       doStuff(() => {
-        $("li:visible").css("height","8%");
+        $("li:visible").css("height","3%");
       });
     });
 
-      $(document).mousemove(function() {
-        doStuff(() => {
-          $yo = $("body").width();
+    $(document).mousemove(function() {
+      doStuff(() => {
+        $yo = $("body").width();
 
-        // if cursor is less than 15% from the left, slide list and expand button // 
+      // if cursor is less than 15% from the left, slide list and expand button // 
 
         if (event.pageX < ($yo * .1)) {
-
           $("ul").addClass("left");
-          $(".expand,.close").css("left","2%");
+          $(".expand,.close,.mail").css("left","2%");
 
         } else {
-
           $("ul").removeClass("left");
-          $(".expand,.close").css("left","-30px");
-
+          $(".expand,.close,.mail").css("left","-30px");
         }
-        })
       });
+    });
 
-      // expand "this" list element //
+    // expand "this" list element //
 
-      $("li", this).click(function() {
+    $("li", this).click(function() {
 
-        doStuff(() => {
-          var h = $(".info", this).outerHeight();
-          var i = $("img", this).outerHeight();
+      doStuff(() => {
+        var h = $(".info", this).outerHeight();
+        var i = $("img", this).outerHeight();
 
-           // if paragraph is shorter than image, use image height //
-        
-          if (h < i ) {
-            $(this).css("height", i);
-          } else {
-            $(this).css("height", h);
-          }
-        });
-
-      });
-
-      // expand all list elements" 
-
-      $(".expand").click(function() {
-
-        doStuff(() => {
-          // get list element heights //
+          // if paragraph is shorter than image, use image height //
       
-          $("li").css("height", function() {
-
-          // compare paragraph to image height again to use larger //
-
-            var h2 = $(".info", this).outerHeight();
-            var i2 = $("img", this).outerHeight();
-            return h2 > i2 ? h2 : i2;
-          });
-
-          $(".close").show();
-          $(".expand").hide();
-        });
+        if (h < i ) {
+           $(this).css("height", i);
+        } else {
+          $(this).css("height", h);
+        }
       });
 
-    // reset all list element heights //
+    });
 
-      $(".close").click(function() {
+    // expand all list elements" 
 
-        doStuff(() => {
-          $("li").css("height","8%");
-          $(".close").hide();
-          $(".expand").show();
+    $(".expand").click(function() {
+
+      doStuff(() => {
+        // get list element heights //
+      
+        $("li").css("height", function() {
+
+        // compare paragraph to image height again to use larger //
+
+          var h2 = $(".info", this).outerHeight();
+          var i2 = $("img", this).outerHeight();
+          return h2 > i2 ? h2 : i2;
         });
 
+        $(".close").show();
+        $(".expand").hide();
+      });
+    });
+
+  // reset all list element heights //
+
+    $(".close").click(function() {
+
+      doStuff(() => {
+        $("li").css("height","3%");
+        $(".close").hide();
+        $(".expand").show();
       });
 
-      $("li",this).mouseover(function() {
-        doStuff(() => {
-          $(".me", this).css("color", "white");
-        });
-      }).mouseout(function() {
-        doStuff(() => {
-          $(".me", this).css("color", "#0078ff");
-        })
-      });
-    
+    });
 
-      doStuff(null, () => {
-        var h3 = $(".info", this).outerHeight();
-        var i3 = $("img", this).outerHeight();
-        
-        $("li", this).css("height", h3 + i3);
-      })
+    $("li",this).mouseover(function() {
+       doStuff(() => {
+        $(".me", this).css("color", "black");
+      });
+    }).mouseout(function() {
+      doStuff(() => {
+        $(".me", this).css("color", "#0078ff");
+      });
+    });
 
       // cycle images of the same class //
 
-      $('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').click(function () {
-        var stuff = () => {
-          $(this).hide();
-          var next = $(this).next();
-          console.log(next.length);
-          if (next.length == 0)
-          next = $(this).parent().find('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').first();
-          next.show();
-        };
-        doStuff(stuff, stuff);
-      });
+    $('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').click(function() {
+         $(this).hide();
+        var next = $(this).next();
+        console.log(next.length);
+        if (next.length == 0)
+        next = $(this).parent().find('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').first();
+        next.show();
+    });
 
 });
 
