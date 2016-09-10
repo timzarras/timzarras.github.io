49fb1f742140cf057b1c8f59ae7d68e2cf47822d
tzarras <TimKitchen@Tims-MBP.home>
2016-09-10 14:44:21 -0400

    yo

--- new/js/timscript2.js
+++ new/js/timscript2.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
 
     // let 'em know who ya are at the core //
 
+
+    if ($(window).width() > 1024) {
+
     setTimeout(function() {
     var i = $(".projectzone:first").outerHeight();
     $("li:first").css("height",i);
     $(".projectzone:first,.info:first,.close:first").show();
+    $(".year:first").css("left","60%");
     },500);
-
-    if ($(window).width() > 1024) {
 
     $(window).resize(function() {
         $("li").css("height","2%");
+        $(".year").css("left","30%");
         $(".close").hide();
         $(".info").hide();
         $(".projectzone").hide();
@@ -21,6 +24,7 @@ $(document).ready(function() {
 
     $(".close",this).click(function() {
         $("li").css("height", "2%");
+        $(".year").css("left","30%");
         $(".close").hide();
         $(".info").hide();
         $(".projectzone").hide();
@@ -34,6 +38,7 @@ $(document).ready(function() {
         var h = $(".info", this).outerHeight();
         var i = $(".projectzone", this).outerHeight();
         var c = $(".close",this);
+        var y =  $(".year",this);
         $(".projectzone",this).show();
         $(".info",this).show();
           // if paragraph is shorter than image, use image height //
@@ -44,7 +49,8 @@ $(document).ready(function() {
         }
         setTimeout(function() {
         $(c).fadeIn();
-        },900);
+        $(y).css("left","60%");
+        },500);
     });
 
     // hey //
@@ -55,6 +61,33 @@ $(document).ready(function() {
     //});
 
       // cycle images of the same class //
+
+    $('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').click(function() {
+         $(this).hide();
+        var next = $(this).next();
+        console.log(next.length);
+        if (next.length == 0)
+        next = $(this).parent().find('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').first();
+        next.show();
+    });
+
+} else {
+
+
+    setTimeout(function() {
+    var h = $(".info", this).outerHeight();
+        var i = $(".projectzone", this).outerHeight();
+        var c = $(".close",this);
+        var y =  $(".year",this);
+        $(".projectzone").show();
+        $(".info").show();
+          // if paragraph is shorter than image, use image height //
+        if (h < i ) {
+           $(this).css("height", i);
+        } else {
+          $(this).css("height", h);
+        }
+    },500);
 
     $('.table,.displayobjects,.heater,.t21i,.chair,.connection,.fan,.TAB').click(function() {
          $(this).hide();
