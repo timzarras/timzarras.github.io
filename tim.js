abdc31e7323c413a9b8210bf30855ccb92483403
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-10-16 17:54:11 -0400

    updates

--- tim.js
+++ tim.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
 Prismic.Api('https://timzarras.prismic.io/api', function (err, Api) {
     Api.form("everything")
     .ref(Api.master())
-    .query(Prismic.Predicates.at("document.type", "project"))                                                                 // how it is in the custom type creator
+    .query(Prismic.Predicates.at("document.type", "project"))
     .submit(function (err, response) {
       var results = response.results;
-      var body = $("body");
+      var body = $(".arch");
       var table = $("table");
 
 
@@ -42,16 +42,17 @@ Prismic.Api('https://timzarras.prismic.io/api', function (err, Api) {
 
       }
 
-      var $divs = $(".row").not(".row:first-child");
+      var $divs = $(".row");
       // sort projects by hidden number value, largest to smallest //
       var ordered = $divs.sort(function (a, b) {
         return $(b).find(".number").text() - $(a).find(".number").text();
       });
       // append reordered divs to project section //
-      $(ordered).appendTo("body");
+      $(ordered).appendTo(".arch");
 
+      $(".row:last").removeClass("row").addClass("last").css({"width":"40vw","margin-left":"0vw"}).appendTo(".upper");
 
-      var $body= $('body').imagesLoaded( function() {
+      var $body= $('.arch').imagesLoaded( function() {
     				$body.masonry({
     	  		// options
     				columnWidth: 50,
@@ -62,23 +63,41 @@ Prismic.Api('https://timzarras.prismic.io/api', function (err, Api) {
     				transitionDuration: '0s',
     				resize: true
     				});
+
+            $(".load").fadeOut(200);
+
        });
 
+
 			if ($(window).width()>750) {
+
+        $(".upper").click(function(){
+          var height = $(window).height();
+          if ($(".upper").height() > height/2){
+            $(".upper").css({"height":"50px","overflow":"hidden"}).scrollTop(0);
+          } else {
+            $(".upper").css({"height":"100vh","overflow":"scroll"});
+          }
+        });
+
+        $(".last > .images > section > .block-img > img").css({"display":"inline","position":"absolute","top":"60px","left":"50vw","z-index":"999 !important"});
+
 
         $("html,body").hide();
 
 				$("html,body").fadeIn(400);
 
+
         $(".row > .images > section:first-child > .block-img > img").show();
         $(".row > .images > section > p").not(".block-img:first-child").hide();
+
 
 					$(".row").click(function(){
 						var height = $(window).height();
 							$(".row").not(this).hide();
 							$(".back").show();
               $(".row > .images > section > p").show();
-							$(".filters").hide();
+							$(".upper,.about").hide();
 							$(this).addClass("white").css({"position":"absolute","top":"0","left":"15vw","width":"60vw"});
 							$("body").animate({scrollTop:0},10);
 								setTimeout(function(){
@@ -90,24 +109,47 @@ Prismic.Api('https://timzarras.prismic.io/api', function (err, Api) {
             $(".row > .images > section > p").not(".block-img:first-child").hide();
 						$("body").animate({scrollTop:0},50);
 						$("img,.back").hide();
-            $(".row > .images > section:first-child > p > img").show()
+            $(".row > .images > section:first-child > p > img,.last > .images > section > .block-img > img").show()
 						$(".row").removeClass("white").css({"position":"relative","top":"auto","left":"auto","width":"25vw"}).show();
-						$(".filters").show();
+						$(".upper,.about").show();
 							$body.masonry('reloadItems');
 	      			$body.masonry('layout');
 					});
 
 				} else {
+          $(".last > .images > section > .block-img > img").css({"display":"inline","position":"relative"});
+          $(".last").css({"left":"7.5%","width":"85%","height":"auto !important"});
           $("body").css("margin-left","0");
-					$(".row").css({"width":"85%","left":"7.5% !important","top":"10%","margin-bottom":"60px","padding-bottom":"40px","border-bottom":"2px solid #e2e2e2"});
+          $(".arch").css({"margin-left":"0","width":"100%"});
+          $(".about").css({"left":"20px","top":"10px"});
+					$(".row").css({"width":"85%","left":"7.5%","top":"10%","margin-bottom":"60px","padding-bottom":"40px","border-bottom":"2px solid #e2e2e2","margin-left":"0"});
 					$("img").show();
+          $(".upper").click(function(){
+            var height = $(window).height();
+            if ($(".upper").height() > height/2){
+              $(".upper").css({"height":"50px","overflow":"hidden"}).scrollTop(0);
+            } else {
+              $(".upper").css({"height":"100%","overflow":"scroll"});
+            }
+          });
         }
 
         if( /Android|webOS|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent) ) {
-
-					$("body").css("margin-left","0");
-					$(".row").css({"width":"85%","left":"7.5% !important","top":"10%","margin-bottom":"60px","padding-bottom":"40px","border-bottom":"2px solid #e2e2e2"});
-					$("img").show();
+          $(".last > .images > section > .block-img > img").css({"display":"inline","position":"relative"});
+          $(".last").css({"left":"7.5%","width":"85%","height":"auto !important"});
+          $("body").css("margin-left","0");
+          $(".arch").css({"margin-left":"0","width":"100%"});
+          $(".about").css({"left":"20px","top":"10px"});
+          $(".row").css({"width":"85%","left":"7.5%","top":"10%","margin-bottom":"60px","padding-bottom":"40px","border-bottom":"2px solid #e2e2e2","margin-left":"0"});
+          $("img").show();
+          $(".upper").click(function(){
+            var height = $(window).height();
+            if ($(".upper").height() > height/2){
+              $(".upper").css({"height":"50px","overflow":"hidden"}).scrollTop(0);
+            } else {
+              $(".upper").css({"height":"100%","overflow":"scroll"});
+            }
+          });
 				} else {
 
         }
@@ -119,8 +161,11 @@ Prismic.Api('https://timzarras.prismic.io/api', function (err, Api) {
 
   }, "MC5XVkFyMUNRQUFEU3M3eU1V.77-9Tu-_vQPvv71Q77-977-977-9cwZF77-977-9LO-_vQ3vv71JMgrvv71u77-977-9FO-_vXHvv71Q77-977-9");
 
-
+  $(".about").click(function(){
+    var height = $(document).height();
+    $("html,body").animate({scrollTop:height},500);
+  });
 
 
 //
-}); // end of document ready //
+});
